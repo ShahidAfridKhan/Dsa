38ca5d9c3a9dc968cecbf85af73689fc3aff9aca
ShahidAfridKhan <shahidafridkhanphatan@gmail.com>
2026-07-06 19:52:38 +0530

    new codes added regarding math

---
+++ src/Prime/TCSPrime.java
@@ -0,0 +1,12 @@
+package Prime;
+
+public class TCSPrime {
+    public static void main(String[] args) {
+        int n=5;
+        int sum=1;
+        for(int i=1;i<=n;i++){
+            sum*=i;
+        }
+        System.out.println(sum);
+    }
+}
---
+++ src/Prime/devisors.java
@@ -0,0 +1,12 @@
+package Prime;
+
+public class devisors {
+    public static void main(String[] args) {
+        int n=10;
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
